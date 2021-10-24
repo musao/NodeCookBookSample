61278f6e4472a62ded52799b436d87992986b15f
musao <yuki-fujii@exa-corp.co.jp>
2021-10-24 17:01:18 +0900

    全角文字が文字化けする問題の修正

--- 1/1.1/server.js
+++ 1/1.1/server.js
@@ -11,12 +11,12 @@ http.createServer(function (request, response) {
 	var lookup = path.basename(decodeURI(request.url));
 	pages.forEach(function(page) {
 		if (page.route === lookup) {
-			response.writeHead(200, {'Content-Type': 'text/html'});
+			response.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
 			response.end(typeof page.output === 'function' ? page.output() : page.output);
 		}
 	});
 	if (!response.finished) {
-		response.writeHead(404);
+		response.writeHead(404, {'Content-Type': 'text/html;charset=utf-8'});
 		response.end('ページがみつかりません！');
 	}
 }).listen(8080);
